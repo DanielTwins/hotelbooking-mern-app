8e3ba46b269a4ca75f3b5a46bfefa3b8c8af3134
DanielTwins <naseri.daniel24@gmail.com>
2024-02-03 16:57:57 +0100

    Add comments to user login route functionality, validation, authentication and JWT token

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -4,41 +4,48 @@ import User from "../models/user";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
+// create an instance of Express Router
 const router = express.Router();
 
-//* create an access token and http cookie
+// define a user login route
 router.post(
   "/login",
   [
+    // use express-validator to validate the request body
     check("email", "Email is required").isEmail(),
-    check("password", "Password with 6 or more characters required").isLength({
+    check("password", "Password is required").isLength({
       min: 6,
     }),
   ],
   async (req: Request, res: Response) => {
-    // check if any errors first
+    // check if there are any validation errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ message: errors.array() });
+      return res.status(400).json({ message: "errors.array()" });
     }
 
-    // if request is good then sending the email and pass using destructuring
+    // destructure email and password from the request body
     const { email, password } = req.body;
 
     try {
-      // fetch the user
+      // find the user in the database based on the provided email
       const user = await User.findOne({ email });
+
+      // if no user found, return an error
       if (!user) {
         return res.status(400).json({ message: "Invalid Credentials" });
       }
 
-      // if there is a user, check the password that matches for the user
+      // compare the provided password with the hashed password stored,
+      //* return bolean value
       const isMatch = await bcrypt.compare(password, user.password);
+
+      // if the password doesn't match, return an error
       if (!isMatch) {
         return res.status(400).json({ message: "Invalid Credentials" });
       }
 
-      // create an access token
+      // create an access token using JWT
       const token = jwt.sign(
         { userId: user.id },
         process.env.JWT_SECRET_KEY as string,
@@ -47,15 +54,19 @@ router.post(
         }
       );
 
+      // set the access token as an HTTP cookie
       res.cookie("auth_token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: process.env.NODE_EMV === "production",
         maxAge: 86400000,
       });
-      res.status(200).json({ userId: user._id }); // _id comes from mongodb docuemnt
+
+      // return the user's ID as a response
+      res.status(200).json({ userId: user._id }); // _id comes from mongoddb document
     } catch (error) {
-      console.log(error); // log the error to backend console
-      // generic error message to client
+      console.log(error); // logs any errors to the backend console
+
+      // send a generic message to the client
       res.status(500).json({ message: "Something went wrong" });
     }
   }
