1b4ad2b68bf07a7f327b8ab81b49ba362da04ce4
DanielTwins <naseri.daniel24@gmail.com>
2024-02-11 16:03:51 +0100

    Create QueryClientProvider using react-query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// create a new instance of QueryClient with custom default options
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0, // set the number of retries to 0 after an error in this case
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    //* wrap the app comp. with QueryClientProvider to provide React Query functionality
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
